8c2c4c004485686df7c2c7c4641d770a60886571
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-01-29 10:43:53 +0200

    Basic frontend

--- WebServer/mysite/realEstateAdvisorWeb/views.py
+++ WebServer/mysite/realEstateAdvisorWeb/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+import sqlite3
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Real estate advisor.")
+     with sqlite3.connect("/home/zoin/workspace/RealEstateAdvisor/realEstate.sqlite") as db:
+        dropdown = ''
+        cursor = db.execute("SELECT Name FROM Districts")
+        for row in cursor:
+            dropdown += '<option value="{0}">{0}</option>'.format(row[0].encode('utf-8'))
+        html = '''<html><body><form align="center">
+                      Area:<br>
+                      <input type="text" name="Area">
+                      <div align="center">
+                       <select name="mydropdown">''' + dropdown + '''</select>
+                      </div>
+                    </form></body></html'''
+        return HttpResponse(html)
